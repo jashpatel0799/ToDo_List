485612f8c588cd4689f1de8cec9c4a4893eeb2e6
jashpatel0799 <jashpatel8561@gmail.com>
2021-05-08 17:56:20 +0530

    Add Various Componemt

---
+++ src/MyComponent/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <>
+        <div className="container">
+            this is an about Component.
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed ipsam 
+                facere, nobis recusandae nemo debitis aperiam tempore deleniti 
+                beatae a voluptas necessitatibus et ea saepe esse in itaque 
+                nesciunt? Sint.</p>
+        </div>
+        </>
+    )
+}
---
+++ src/MyComponent/Todos.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {TodoItems} from "./TodoItems";
+
+export const Todos = (props) => {
+    let myStyle={
+        minHeight: "70vh",
+        margin: "10px auto"
+    }
+    return (
+        <>
+        <div className= "container" style={myStyle}>
+            <h3 className="text">Todos List</h3>
+            {props.todos.length===0? "No Todos to display":
+                props.todos.map((todo) => {
+                return <TodoItems todo = {todo} key={todo.sno} onDelete={props.onDelete}/>
+                })
+            }
+        </div>
+        </>
+    )
+}
